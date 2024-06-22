ee4cef76bf6f25ae722864d75fbfdfa3c905ecc0
gomess99 <gomess99@outlook.com>
2024-06-21 23:10:19 -0300

    Finishing the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -80,8 +80,52 @@ const Navbar = () => {
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">{navItems}</ul>
         </div>
+        {/*btn seach*/}
         <div className="navbar-end">
-          <a className="btn bg-green rouded-full px-6 text-white flex items-center gap-2"><FiPhoneCall/> Contact</a>
+          <button className="btn btn-ghost btn-circle hidden lg:flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </button>
+          {/*btn itens*/}
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle m-3 lg:flex items-center hidden justify-center"
+          >
+            <div className="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+              </svg>
+              <span className="badge badge-sm indicator-item">8</span>
+            </div>
+          </div>
+          {/*btn contact*/}
+          <a className="btn bg-green rouded-full px-6 text-white flex items-center gap-2">
+            <FiPhoneCall /> Contact
+          </a>
         </div>
       </div>
     </header>
