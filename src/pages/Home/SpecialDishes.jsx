5154e2b4096d24eea6a8ae059f478f7bc3f1e54d
gomess99 <gomess99@outlook.com>
2024-06-24 20:23:55 -0300

    Starting the Special Dishes Carousel

---
+++ src/pages/Home/SpecialDishes.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SpecialDishes = () => {
+  return (
+    <div className='section-container my-20'>
+      <div className="text-left">
+        <p className="subtitle">Special Dishes</p>
+        <h3 className="title md:w-[520px]">Standout Dishes from Our Menu</h3>
+      </div>
+    </div>
+  )
+}
+
+export default SpecialDishes
