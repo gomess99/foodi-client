e643819f158553c6508ef1e249a92001488ef139
gomess99 <gomess99@outlook.com>
2024-07-02 08:51:35 -0300

    Finishing the Slick Carousel

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,18 +1,37 @@
 /* eslint-disable react/prop-types */
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { FaHeart } from "react-icons/fa";
 
 const Cards = ({ item }) => {
+  const [isHeartFillted, setIsHeartFillted] = useState(false);
+
+  const handleHeartClick = () => {
+    setIsHeartFillted(!isHeartFillted);
+  };
+
   return (
-    <div className="card bg-base-100 w-96 shadow-xl">
+    <div className="card bg-base-100 w-96 shadow-xl relative">
+      <div
+        className={`rating gap-1 absolute right-2 top-2 p-4 hearStar bg-green ${
+          isHeartFillted ? "text-rose-500" : "text-white"
+        }`}
+        onClick={handleHeartClick}
+      >
+        <FaHeart className="h-5 w-5 cursor-pointer" />
+      </div>
       <Link to={`menu/${item._id}`}>
         <figure>
-          <img src={item.image} alt="" 
-          className="hover:scale-105 transition-all duration-200 md:h-72"/>
+          <img
+            src={item.image}
+            alt=""
+            className="hover:scale-105 transition-all duration-200 md:h-72"
+          />
         </figure>
       </Link>
       <div className="card-body">
-        <h2 className="card-title">{item.name}</h2>
+      <Link to={`menu/${item._id}`}>
+      <h2 className="card-title">{item.name}</h2></Link>
         <p>Description of the item</p>
         <div className="card-actions justify-between items-center mt-2">
           <h5 className="font-semibold">
--- src/pages/Home/SpecialDishes.jsx
+++ src/pages/Home/SpecialDishes.jsx
@@ -3,6 +3,34 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import Cards from "../../components/Cards/Cards";
+import {} from "react-icons/fa6";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+
+const simpleNextArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "red" }}
+      onClick={onClick}
+    >
+      Next
+    </div>
+  );
+};
+
+const simplePrevArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "green" }}
+      onClick={onClick}
+    >
+      Back
+    </div>
+  );
+};
 
 const SpecialDishes = () => {
   const [recipes, setRecipes] = useState([]);
@@ -13,8 +41,8 @@ const SpecialDishes = () => {
     fetch("/menu.json")
       .then((res) => res.json())
       .then((data) => {
-        const specials = data.filter((item) => item.category == "popular")
-        setRecipes(specials)
+        const specials = data.filter((item) => item.category == "popular");
+        setRecipes(specials);
       });
   }, []);
 
@@ -23,8 +51,8 @@ const SpecialDishes = () => {
     dots: true,
     infinite: false,
     speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToShow: 3,
+    slidesToScroll: 3,
     initialSlide: 0,
     responsive: [
       {
@@ -52,21 +80,38 @@ const SpecialDishes = () => {
         },
       },
     ],
+    nextArrow: <simpleNextArrow />,
+    prevArrow: <simplePrevArrow />,
   };
 
   return (
-    <div className="section-container my-20">
+    <div className="section-container my-20 relative">
       <div className="text-left">
         <p className="subtitle">Special Dishes</p>
         <h3 className="title md:w-[520px]">Standout Dishes from Our Menu</h3>
       </div>
 
-      <Slider {...settings}>
-        {
-            recipes.map((item, i) =>(
-                <Cards key={i} item={item}/>
-            ))
-        }
+
+        {/* arrow btn */}
+      <div className="md:absolute right-3 top-8 mb-10 md:mr-24">
+        <button
+          onClick={() => slider?.current?.slickPrev()}
+          className="btn p-2 rounded-full ml-5"
+        >
+          <FaAngleLeft className="w-8 h-8 p-1" />
+        </button>
+        <button
+          onClick={() => slider?.current?.slickNext()}
+          className="btn p-2 rounded-full ml-5 bg-green"
+        >
+          <FaAngleRight className="w-8 h-8 p-1" />
+        </button>
+      </div>
+
+      <Slider ref={slider} {...settings} className="overflow-hidden mt-10 space-x-5">
+        {recipes.map((item, i) => (
+          <Cards key={i} item={item} />
+        ))}
       </Slider>
     </div>
   );
